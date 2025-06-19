b056d1bd1c4bd6a4a6fd3aeb79d1f23225b7746f
Komal Choudhary <komalchoudharyc3636@gmail.com>
2025-06-19 23:27:55 +0530

    Create Sort_colours.cpp

---
+++ DSA/18 June/Sort_colours.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int count0 = 0, count1 = 0, count2 = 0;
+
+        // Count occurrences of 0s, 1s, and 2s
+        for (int num : nums) {
+            if (num == 0) count0++;
+            else if (num == 1) count1++;
+            else count2++;
+        }
+
+        // Overwrite original array based on the counts
+        int index = 0;
+        for (int i = 0; i < count0; i++) nums[index++] = 0;
+        for (int i = 0; i < count1; i++) nums[index++] = 1;
+        for (int i = 0; i < count2; i++) nums[index++] = 2;
+    }
+};
